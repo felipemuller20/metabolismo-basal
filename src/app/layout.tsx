a3cc9a5c76acfd78f4f3807a6ed2d08b1b9868ee
felipe <felipeavmuller@gmail.com>
2024-03-18 18:07:57 -0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-neutral-50 text-neutral-800`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [gender, setGender] = useState<"male" | "female">("male");
+  const [height, setHeight] = useState<number>(0);
+  const [weight, setWeight] = useState<number>(0);
+  const [age, setAge] = useState<number>(0);
+  const [result, setResult] = useState<number>(0);
+  function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    event?.preventDefault();
+
+    let formResult;
+    if (gender === "male") {
+      formResult = 66 + 13.7 * weight + 5 * height - 6.8 * age;
+      setResult(formResult);
+      return;
+    }
+
+    formResult = 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
+    setResult(formResult);
+    return;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="text-center mx-auto">
+      <header className="m-5 flex items-center justify-center flex-col">
+        <h1 className="text-3xl bold">Calcule a sua</h1>
+        <h2 className="text-2xl bold text-neutral-700">
+          Taxa de Metabolismo Basal<span className="text-sm">*</span>
+        </h2>
+        <div className="text-sm text-neutral-500 w-52 mt-1">
+          <p>
+            Descubra quantas calorias o seu corpo consome, segundo a equação de
+            <span className="bold text-neutral-700"> Harris-Benedict</span>.
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </header>
+      <main className="flex items-center justify-center">
+        <form className="rounded-xl shadow-lg m-5 bg-[#eeeded] lg:w-1/3 w-fit p-10 text-start flex flex-col gap-5 text-neutral-500">
+          <label>
+            Sexo
+            <section className="flex justify-normal md:gap-10 gap-4 text-neutral-800">
+              <div className="flex gap-1">
+                <input
+                  type="radio"
+                  id="male"
+                  name="gender"
+                  value="male"
+                  checked={gender === "male"}
+                  onChange={() => setGender("male")}
+                />
+                <label htmlFor="male">Masculino</label>
+              </div>
+              <div className="flex gap-1">
+                <input
+                  type="radio"
+                  id="female"
+                  name="gender"
+                  value="female"
+                  checked={gender === "female"}
+                  onChange={() => setGender("female")}
+                />
+                <label htmlFor="female">Feminino</label>
+              </div>
+            </section>
+          </label>
+          <section className="flex gap-5 flex-wrap">
+            <label className="text-sm">
+              {`Altura (cm)`}
+              <section className="flex gap-10 mt-1 text-neutral-800">
+                <input
+                  type="number"
+                  value={height}
+                  className="p-2 rounded border border-neutral-400 w-20"
+                  onChange={(event) => {
+                    const value = Number(event.target.value);
+                    if (value >= 0) {
+                      setHeight(value);
+                    }
+                  }}
+                />
+              </section>
+            </label>
+            <label className="text-sm">
+              {`Peso (kg)`}
+              <section className="flex gap-10 mt-1 text-neutral-800">
+                <input
+                  type="number"
+                  value={weight}
+                  className="p-2 rounded border border-neutral-400 w-20"
+                  onChange={(event) => {
+                    const value = Number(event.target.value);
+                    if (value >= 0) {
+                      setWeight(value);
+                    }
+                  }}
+                />
+              </section>
+            </label>
+            <label className="text-sm">
+              {`Idade`}
+              <section className="flex gap-10 mt-1 text-neutral-800">
+                <input
+                  type="number"
+                  value={age}
+                  className="p-2 rounded border border-neutral-400 w-20"
+                  onChange={(event) => {
+                    const value = Number(event.target.value);
+                    if (value >= 0) {
+                      setAge(value);
+                    }
+                  }}
+                />
+              </section>
+            </label>
+          </section>
+          <button
+            onClick={handleClick}
+            className="rounded-lg border border-neutral-700 disabled:border-red-400 hover:bg-neutral-300 disabled:hover:bg-red-200 text-gray-900 text-sm w-fit px-5 py-3"
+            disabled={height <= 0 || weight <= 0 || age <= 0}
+          >
+            Calcular
+          </button>
+        </form>
+      </main>
+      <section className="flex flex-col items-center justify-center gap-2">
+        <h2 className="text-2xl bold text-neutral-700">
+          Taxa de Metabolismo Basal
+        </h2>
+        <p className="text-4xl bold text-neutral-800">
+          {result.toFixed(2)} kcal
+        </p>
+      </section>
+      <p className="text-xs text-neutral-500 w-96 mx-auto mt-10">
+        <span className="text-neutral-700">*</span>Taxa de Metabolismo Basal é a
+        quantidade de energia que o seu corpo consome quando está em repouso
+        absoluto.
+      </p>
     </main>
   );
 }
